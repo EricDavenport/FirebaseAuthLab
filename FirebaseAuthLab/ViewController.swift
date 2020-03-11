96c109341a47874a641710e1e32aac62923a01c1
Eric Davenport <EricDavenport@pursuit.org>
2020-03-10 23:19:00 -0400

    Initial Commit

---
+++ FirebaseAuthLab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseAuthLab
+//
+//  Created by Eric Davenport on 3/10/20.
+//  Copyright © 2020 Eric Davenport. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
